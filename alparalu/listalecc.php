a441caebb9ff5d38e1b43e34544d92329fcc3c0a
vasquezalvaro <vasquezp@gmail.com>
2016-10-18 20:18:52 -0500

    Add files via upload

---
+++ alparalu/listalecc.php
@@ -0,0 +1,225 @@
+<?php ob_start();?>
+<html lang="en-US"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="http://avp.colombianproducts.net/alparalu/xmlrpc.php">
+
+	<title>Lista de Lecciones de Alemán</title>
+<link rel="dns-prefetch" href="http://fonts.googleapis.com/">
+<link rel="dns-prefetch" href="http://s.w.org/">
+<link rel="alternate" type="application/rss+xml" title="Alemán para Lu » Feed" href="http://avp.colombianproducts.net/alparalu/feed/">
+<link rel="alternate" type="application/rss+xml" title="Alemán para Lu » Comments Feed" href="http://avp.colombianproducts.net/alparalu/comments/feed/">
+<link rel="alternate" type="application/rss+xml" title="Alemán para Lu » Bienvenida Lu Comments Feed" href="http://avp.colombianproducts.net/alparalu/sample-page/feed/">
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/avp.colombianproducts.net\/wpwebsite\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.6.1"}};
+			!function(a,b,c){function d(a){var c,d,e,f,g,h=b.createElement("canvas"),i=h.getContext&&h.getContext("2d"),j=String.fromCharCode;if(!i||!i.fillText)return!1;switch(i.textBaseline="top",i.font="600 32px Arial",a){case"flag":return i.fillText(j(55356,56806,55356,56826),0,0),!(h.toDataURL().length<3e3)&&(i.clearRect(0,0,h.width,h.height),i.fillText(j(55356,57331,65039,8205,55356,57096),0,0),c=h.toDataURL(),i.clearRect(0,0,h.width,h.height),i.fillText(j(55356,57331,55356,57096),0,0),d=h.toDataURL(),c!==d);case"diversity":return i.fillText(j(55356,57221),0,0),e=i.getImageData(16,16,1,1).data,f=e[0]+","+e[1]+","+e[2]+","+e[3],i.fillText(j(55356,57221,55356,57343),0,0),e=i.getImageData(16,16,1,1).data,g=e[0]+","+e[1]+","+e[2]+","+e[3],f!==g;case"simple":return i.fillText(j(55357,56835),0,0),0!==i.getImageData(16,16,1,1).data[0];case"unicode8":return i.fillText(j(55356,57135),0,0),0!==i.getImageData(16,16,1,1).data[0];case"unicode9":return i.fillText(j(55358,56631),0,0),0!==i.getImageData(16,16,1,1).data[0]}return!1}function e(a){var c=b.createElement("script");c.src=a,c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g,h,i;for(i=Array("simple","flag","unicode8","diversity","unicode9"),c.supports={everything:!0,everythingExceptFlag:!0},h=0;h<i.length;h++)c.supports[i[h]]=d(i[h]),c.supports.everything=c.supports.everything&&c.supports[i[h]],"flag"!==i[h]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[i[h]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script><script src="alparalu_files/wp-emoji-release.js" type="text/javascript"></script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel="stylesheet" id="academica-style-css" href="alparalu_files/style.css" type="text/css" media="all">
+<link rel="stylesheet" id="academica-style-mobile-css" href="alparalu_files/media-queries.css" type="text/css" media="all">
+<link rel="stylesheet" id="academica-google-font-default-css" href="alparalu_files/css.css" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="alparalu_files/dashicons.css" type="text/css" media="all">
+<script type="text/javascript" src="alparalu_files/jquery.js"></script>
+<script type="text/javascript" src="alparalu_files/jquery-migrate.js"></script>
+<link rel="https://api.w.org/" href="http://avp.colombianproducts.net/alparalu/wp-json/">
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://avp.colombianproducts.net/alparalu/xmlrpc.php?rsd">
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://avp.colombianproducts.net/alparalu/wp-includes/wlwmanifest.xml"> 
+<meta name="generator" content="WordPress 4.6.1">
+<link rel="canonical" href="http://avp.colombianproducts.net/alparalu/">
+<link rel="shortlink" href="http://avp.colombianproducts.net/alparalu/">
+<link rel="alternate" type="application/json+oembed" href="http://avp.colombianproducts.net/alparalu/wp-json/oembed/1.0/embed?url=http%3A%2F%2Favp.colombianproducts.net%2Fwpwebsite%2F">
+<link rel="alternate" type="text/xml+oembed" href="http://avp.colombianproducts.net/alparalu/wp-json/oembed/1.0/embed?url=http%3A%2F%2Favp.colombianproducts.net%2Fwpwebsite%2F&amp;format=xml">
+<!-- AAVVPP -->
+<script type="text/javascript" src="alparalu_files/avpfunctions.js"></script>
+
+</head>
+
+<body onload="if (location.href.indexOf('reload')==-1) location.replace(location.href+'?reload');" class="home page page-id-2 page-template-default column-double">
+	
+	
+	<div id="header" class="clearfix">
+
+		
+			<div class="inner-wrap">
+
+				<div id="logo" class="logo-left">
+					
+						<a href="http://avp.colombianproducts.net/alparalu/alparalu.html" title="Alemán para Lu - " rel="home">
+
+							<img id="logoimg" src="alparalu_files/berlin600.jpg" alt="Alemán para Lu" height="150" width="150">
+						</a>
+
+					
+					<p id="site-description"></p>
+				</div><!-- end #logo -->
+
+			</div><!-- end .inner-wrap -->
+
+			<div id="main-nav">
+
+				<div class="inner-wrap">
+
+					<nav class="main-navbar" role="navigation">
+
+	                    <div class="navbar-header">
+	                        
+	                    </div>
+
+
+	                    <div id="navbar-main">
+
+	                        
+
+	                    </div><!-- #navbar-main -->
+
+	                </nav><!-- .navbar -->
+<!-- AAVVPP
+					<div id="search">
+						<form method="get" id="searchform" action="http://avp.colombianproducts.net/alparalu/">
+	<label for="s" class="assistive-text hidden">Search</label>
+	<input id="s" name="s" placeholder="Search" type="text">
+	<button id="searchsubmit" name="submit" type="submit">Search</button>
+</form>					</div>
+-->
+<!-- end #search -->
+
+				</div><!-- end .inner-wrap -->
+
+			</div><!-- end #main-nav -->
+
+		
+	</div><!-- end #header -->
+
+	<div id="wrap">
+<div id="content" class="clearfix">
+
+
+	<div class="column column-title">
+<!-- AAVVPP 	
+		<div id="crumbs">
+	<p></p>
+</div>  --><!-- end #crumbs --> 	
+
+</div><!-- end .column-title -->
+
+	<div class="column column-narrow">
+		<div id="nav_menu-2" class="widget clearfix widget_nav_menu"><h3 class="heading">Cursos de Alemán</h3>
+		<div class="menu-menu1-container"><ul id="menu-menu1" class="menu">
+<li id="menu-item-30" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-2 current_page_item menu-item-30"><a href="http://avp.colombianproducts.net/alparalu/alparalu.html">Home</a></li>
+<li id="menu-item-19" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-19"><a href="http://avp.colombianproducts.net/alparalu/listalecc.php">Lista de Lecciones</a></li>
+<!-- AAVVPP <li id="menu-item-27" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-27"><a href="http://avp.colombianproducts.net/alparalu/coffeebg.html">Coffee Break German</a></li>
+--> 
+<li id="menu-item-36" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-36"><a href="http://avp.colombianproducts.net/alparalu/otroslinks.html">Otros links útiles</a></li>
+</ul></div></div>	</div><!-- end .column-narrow -->
+
+	<div class="column column-content single">
+
+
+		
+<div id="post-2" class="post-2 page type-page status-publish hentry">
+
+
+</div><!-- end #post-## -->
+<div id="comments">
+
+
+</div><!-- #comments -->
+
+
+<!-- AAVVPP -->
+<h2>Lista de Lecciones de Alemán</h2>
+Seleccione cada lección y descargue su documento en PDF y/o su sonido en mp3</br>
+<?php
+
+include 'conectarseaBD.php';
+mostrarcursos (0);
+
+ 
+function mostrarcursos($activ){  
+
+$dbhandle = Conectarbillar();
+
+		echo '<div class="listac">';                
+        echo'<ul style="font-size:75%">'. "\n";
+        // AAVVPP SELECT ..... WHERE =     OR   WHERE != ************************************** 
+		$result = $dbhandle->query('SELECT * FROM Lecciones WHERE Activ=' . $activ . ';');
+		$j=1;
+		$codant="";
+		foreach($result as $row) {
+			$cod=$row['Codigo'];
+			if ($cod != $codant) {	
+				switch ($cod) {
+    				case "WN":
+        				echo '<p style="font-size:150%">DW Warum Nicht?</p>';
+        				break;
+    				case "AT":
+        				echo '<p style="font-size:150%">DW Audio Tutor</p>';
+        				break;
+    				case "RD":
+        				echo '<p style="font-size:150%">DW Radio D Series 1</p>';
+        			break;
+        			case "CB":
+        				echo '<p style="font-size:150%">CB Coffee Break German</p>';
+        			break;
+    				default:
+        				echo "\n";
+				}
+         		
+			}
+			
+			$PDF='<a href=' . $row['PDF'] . ' target="_blank" style="visibility: hidden;" id=PDF-' . $j . '></a>';
+			$MP3='<a href=' . $row['MP3'] . ' target="_blank" style="visibility: hidden;" id=MP3-' . $j . '></a>';
+			$forma='<form  action = "#">';
+			if ($cod != "CB") {	
+				$chkb='<input id=' . $j . ' type="checkbox" value="false"  onclick = "descargas(' . $j . ')" />';
+			} else {
+				$chkb='<input id=' . $j . ' type="checkbox" value="false"  onclick = "descargasCB(' . $j . ')" />';
+				$MP3='<a href=' . $row['MP3'] . ' target="_blank"  download="" style="visibility: hidden;" id=MP3-' . $j . '></a>';
+				// AAVVPP <a href="alparalu_files/CB-27.mp3" download="">Link text CB-27.mp3</a></p>
+			}
+			echo '<li>' .  $forma . $chkb . $row['Codigo'] . '-' . trim($row['LeccNom']) . '&nbsp;&nbsp;&nbsp;&nbsp;';
+			echo $PDF  . '&nbsp;&nbsp;&nbsp;&nbsp;'. $MP3 . '</form></li>' . "\n";
+			// echo $PDF . '&nbsp;&nbsp;**' . $MP3 . "\n";
+			$j++;
+			$codant=$cod;
+		}
+		echo'</ul>'. "\n";
+		echo '</div> <!-- listac -->'. "\n"; 
+
+
+}//end function 
+?>	
+	
+	</div><!-- end .column-content -->
+
+	<div class="column column-narrow column-last">
+			</div><!-- end .column-narrow -->
+
+	
+</div><!-- end #content -->
+
+
+		</div><!-- end #wrap -->
+
+		<script type="text/javascript" src="alparalu_files/jquery_002.js"></script>
+<script type="text/javascript" src="alparalu_files/superfish.js"></script>
+<script type="text/javascript" src="alparalu_files/functions.js"></script>
+<script type="text/javascript" src="alparalu_files/wp-embed.js"></script>
+	
+</body></html>
